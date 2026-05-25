ce7aaf06fef17a7aa0f0b2d49563566a3a5172fe
jonghapnoriteo <rose878455@gmail.com>
2026-05-25 09:06:54 +0900

    Change temporary native folder name to 'SFNatives'

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -202,7 +202,7 @@ exports.isFirstLaunch = function(){
  * @returns {string} The name of the folder.
  */
 exports.getTempNativeFolder = function(){
-    return 'WCNatives'
+    return 'SFNatives'
 }
 
 // System Settings (Unconfigurable on UI)
